f82daf84ea714183c7078158788eb4f8e91e6043
kotasuke55 <uverock5@gmail.com>
2022-06-11 00:51:12 +0900

    f

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $items = Todo::all();
+         return view('index', ['items'=>$items]);
+    }
+    public function create(Request $request)
+    {
+       $this->validate($request,Todo::$rules);
+       $form = $request->all();
+       Todo::create($form);
+       return redirect('/');
+    }
+    public function update(Request $request)
+    {
+        $this->validate($request,Todo::$rules);
+        $form = $request->all();
+        unset($form['_token']);
+        Todo::where('id',$request->id)->update($form);
+        return redirect('/');
+    }
+    public function remove(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link href="{{asset('/assets/css/style.css')}}" rel="stylesheet">
+ <link href="{{asset('/assets/css/reset.css')}}" rel="stylesheet">
+
+  <title>Document</title>
+</head>
+<body>
+  @if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<div class="todo">
+  <div class="todo__list">
+    <p class="todo__ttl">Todo List</p>
+    <form action="/todo/create" method="post">
+      @csrf
+      <input type="text" name="content" class="create">
+      <button class="create__btn">追加</button>
+    </form>
+    <table>
+      <tr>
+        <th>作成日</th>
+        <th>タスク名</th>
+        <th>更新</th>
+        <th>削除</th>
+      </tr>
+      @foreach($items as $item)
+      <tr>
+        <td> {{$item->created_at}}</td>
+        <td> <input type="text" value="{{$item->content}}" class="content" ></td>
+        <td>
+          <form action="/todo/update" method="post">
+            @csrf
+          <button>更新</button></form>
+        </td>
+        <td>
+          <form action="/todo/delete" method="post">@csrf
+            <button>削除</button></form></td>
+      </tr>
+      @endforeach
+    </table>
+  </div>
+</div>
+  
+</body>
+</html>
